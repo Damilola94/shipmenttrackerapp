6f097b799b74f6e9da0f00a8e1a224d67f6b49ec
Damilola Ogunboyejo <demilademichael18@gmail.com>
2024-06-05 13:27:41 +0100

    Done!

--- src/screen/WalletScreen/WalletScreen.tsx
+++ src/screen/WalletScreen/WalletScreen.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const WalletScreen = () => {
-  return (
-    <View style={styles.root}>
-      <Text>WalletScreen</Text>
-    </View>
-  );
-};
+const WalletScreen = () => <View style={styles.root}><Text>WalletScreen</Text></View>
 
 const styles = StyleSheet.create({
   root: {
